515c127bfc79330f6c4fc43762c1abe9868a61bd
andrew-merz <merz.school@gmail.com>
2022-04-26 16:37:55 -0400

    auth_controller updated, server.js updated, User.js updated

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const router = express.Router();
-// const bcrypt = require("bcryptjs");
-// const { User } = require("../models");
+const bcrypt = require("bcryptjs");
+const { User } = require("../models");
 
 router.get("/register", function (req, res) {
-    //return res.render("auth/register");
-    res.send('register page')
+  //return res.render("auth/register");
+  res.send("register page");
 });
 
 router.get("/login", function (req, res) {
-    res.send('login page')
+  res.send("login page");
 });
 
 //add router.post and router.get later
 
 module.exports = router;
 
-app.get("/", (request, response) => 
-response.send("Welcome to Twitter!"));
+router.get("/", (request, response) => response.send("Welcome to Twitter!"));
