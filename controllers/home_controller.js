88c74dc7bafd91f2f429c0f4b18ad90243cfdcef
Kunhyuk <klee762@gatech.edu>
2022-04-26 15:16:55 -0400

    setting up controllers

---
+++ controllers/home_controller.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router()
+
+const db = require('../models')
+
+router.get('/', async (req, res, next) => {
+
+});
+
+module.exports = router
